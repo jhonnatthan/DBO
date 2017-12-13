c59c230304b101a2c366cc54ed1d083bcceee568
Jhonnatthan <j-jhoe@hotmail.com>
2017-12-13 02:32:52 -0200

    Css fix

--- app/Views/login/formlogin.php
+++ app/Views/login/formlogin.php
@@ -1,6 +1,6 @@
 <div class="container-fluid h-100">
     <div class="row h-100 justify-content-center align-items-center">
-        <div class="col-md-5 col-xs-12 p-4 m-1 mb-4 bg-white rounded shadow-xs animated fadeInLeft">
+        <div class="col-md-5 col-xs-12 p-4 m-1 mb-4 bg-white rounded shadow-md animated fadeInLeft">
             <p class="h2 text-primary text-center">Entrar</p>
             <form class="mt-3" id="frmlog">
                 <div class="form-group">
@@ -20,10 +20,10 @@
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Acessar <i class="fa fa-check" aria-hidden="true"></i></button>
             </form>
         </div>
-        <div class="col-md-4 col-xs-12 p-5 m-1 mb-4 bg-primary text-white rounded align-self-center shadow-xs animated fadeInRight">
+        <div class="col-md-4 col-xs-12 p-5 m-1 mb-4 bg-white text-primary rounded align-self-center shadow-md animated fadeInRight">
             <p class="h2 text-center mb-4">Precisa de ajuda <i class="fa fa-question"></i></p>
-            <button class="btn btn-md btn-light btn-block mt-2" type="button" id="solicitarAcesso">Solicitar acesso <i class="fa fa-user-plus" aria-hidden="true"></i></button>
-            <button class="btn btn-md btn-light btn-block mt-2" type="button" >Reportar erro <i class="fa fa-bug" aria-hidden="true"></i></button>
+            <button class="btn btn-md btn-primary btn-block mt-2" type="button" id="solicitarAcesso">Solicitar acesso <i class="fa fa-user-plus" aria-hidden="true"></i></button>
+            <button class="btn btn-md btn-primary btn-block mt-2" type="button" >Reportar erro <i class="fa fa-bug" aria-hidden="true"></i></button>
         </div>
     </div>
 </div>
