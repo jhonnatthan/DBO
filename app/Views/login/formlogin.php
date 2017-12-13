1eb65e433f9bcc0a9bccf46065abda26b2f943a8
Jhonnatthan <j-jhoe@hotmail.com>
2017-12-13 01:57:58 -0200

    Modal changed to Swal

--- app/Views/login/formlogin.php
+++ app/Views/login/formlogin.php
@@ -1,18 +1,15 @@
 <div class="container-fluid h-100">
     <div class="row h-100 justify-content-center align-items-center">
-        <div class="col-md-5 col-xs-12 p-4 mb-4 bg-white rounded shadow-xs animated fadeInLeft">
+        <div class="col-md-5 col-xs-12 p-4 m-1 mb-4 bg-white rounded shadow-xs animated fadeInLeft">
             <p class="h2 text-primary text-center">Entrar</p>
-            <form id="frmlog">
+            <form class="mt-3" id="frmlog">
                 <div class="form-group">
-                    <label>Login</label>
                     <input type="text" name="login" class="form-control" placeholder="Login" maxlength="50" required autofocus>
                 </div>
                 <div class="form-group">
-                    <label>Senha</label>
                     <input type="password" name="senha" class="form-control" placeholder="Senha" maxlength="50" required>
                 </div>
                 <div class="form-group">
-                    <label>Cargo</label>
                     <select class="form-control">
                         <option value="4">Aluno</option>
                         <option value="3">Orientador</option>
@@ -23,11 +20,10 @@
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Acessar <i class="fa fa-check" aria-hidden="true"></i></button>
             </form>
         </div>
-        <div class="col-md-4 col-xs-12 p-5 mb-4 bg-primary text-white rounded align-self-center shadow-xs animated fadeInRight">
+        <div class="col-md-4 col-xs-12 p-5 m-1 mb-4 bg-primary text-white rounded align-self-center shadow-xs animated fadeInRight">
             <p class="h2 text-center mb-4">Precisa de ajuda <i class="fa fa-question"></i></p>
             <button class="btn btn-md btn-light btn-block mt-2" type="button" id="solicitarAcesso">Solicitar acesso <i class="fa fa-user-plus" aria-hidden="true"></i></button>
             <button class="btn btn-md btn-light btn-block mt-2" type="button" >Reportar erro <i class="fa fa-bug" aria-hidden="true"></i></button>
-            <button class="btn btn-md btn-light btn-block mt-2" type="button" >Enviar e-mail <i class="fa fa-envelope" aria-hidden="true"></i></button>
         </div>
     </div>
 </div>
--- js/login.js
+++ js/login.js
@@ -1,7 +1,5 @@
 $(function () {
 
-    // Login
-
     $('#frmlog').submit( function () {
         var dados = $(this).serialize();
 
@@ -13,9 +11,33 @@ $(function () {
     });
 
     $('#solicitarAcesso').click( function () {
-        $('#modalContent').load('/solicitar-acesso', function () {
-            $('#modal').modal('show');
+
+        swal({
+            title: 'Solicitar acesso',
+            input: 'email',
+            confirmButtonText: 'Enviar <i class="fa fa-send"></i>',
+            showLoaderOnConfirm: true,
+            preConfirm: function (email) {
+                return new Promise(function (resolve) {
+                    setTimeout(function () {
+                        if(email == "adm@adm.com") {
+                            swal.showValidationError('This email is already taken.');
+                        }
+                        resolve();
+                    }, 2000);
+                });
+            },
+            allowOutsideClick: false
+        }).then( function (result) {
+            if(result.value) {
+                swal({
+                    type: 'success',
+                    title: 'Ajax request finished',
+                    html: 'Submitted e-mail: ' + result.value
+                })
+            }
         });
+
     });
 
     function sendPost(n,o){$.post("/auth",n,function(n){o(n)},"json").fail(function(n,o,s){console.log(n.responseText)})}
